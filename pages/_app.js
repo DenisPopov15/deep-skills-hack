e715e1d100f534bfa17a0bb31f7d93f28a2b2de4
Denis <denis@slatestudio.com>
2021-10-23 21:12:46 +0100

    Add projects page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
+// import '../styles/Projects.module.css'
 import '../styles/globals.css'
-import styles from '../styles/App.module.css'
+// import styles from '../styles/App.module.css'
 import CeramicClient from '@ceramicnetwork/http-client'
 import { useEffect, useState, Fragment } from 'react'
 import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
@@ -8,6 +9,7 @@ import { TileDocument } from '@ceramicnetwork/stream-tile'
 import { DID } from 'dids'
 import DataModels from './components/DataModels'
 import Image from 'next/image'
+import Projects from './projects/index'
 // import { Core } from '@self.id/core'
 
 import { API_URL } from './config/index'
@@ -21,7 +23,7 @@ function MyApp() {
   const [ethAddresses, setEthAddresses] = useState();
   const [ethereum, setEthereum] = useState();
   const [commits, setCommits] = useState([]);
-  const [appStarted, setAppStarted] = useState(false);
+  // const [appStarted, setAppStarted] = useState(false);
 
   useEffect(() => {
     if(window.ethereum) {
@@ -61,148 +63,152 @@ function MyApp() {
     }
   }, [ethereum, ethAddresses]);
 
-  function getTestDocUI(testDoc, streamId) {
-    let content = <h3>Test doc loading...</h3>
-    let commitsUI = [];
-    for(let commit of commits) {
-      commitsUI.push(<div>{JSON.stringify(commit)}</div>);
-    }
+  // function getTestDocUI(testDoc, streamId) {
+  //   let content = <h3>Test doc loading...</h3>
+  //   let commitsUI = [];
+  //   for(let commit of commits) {
+  //     commitsUI.push(<div>{JSON.stringify(commit)}</div>);
+  //   }
 
-    if(testDoc && streamId) {
-      content = <div className={styles.streamTestPanel}>
-        <h2>Tests On Basic Streams</h2>
-        <h3>Test doc: {testDoc}</h3>
-        <div> {streamId} </div>
-        <h3>Test doc from load: {loadedDoc}</h3>
-        <h3>Test doc after update: {updatedDoc}</h3>
-        <div>
-          <h3>Commits: </h3>
-          <div>
-            {commitsUI}
-          </div>
-        </div>
-      </div>;
-    }
-    return content
-  }
+  //   if(testDoc && streamId) {
+  //     content = <div className={styles.streamTestPanel}>
+  //       <h2>Tests On Basic Streams</h2>
+  //       <h3>Test doc: {testDoc}</h3>
+  //       <div> {streamId} </div>
+  //       <h3>Test doc from load: {loadedDoc}</h3>
+  //       <h3>Test doc after update: {updatedDoc}</h3>
+  //       <div>
+  //         <h3>Commits: </h3>
+  //         <div>
+  //           {commitsUI}
+  //         </div>
+  //       </div>
+  //     </div>;
+  //   }
+  //   return content
+  // }
 
-  function getEthNeededPanel() {
-    return <div className="csn-eth-panel">
-      You need wallet or you will stay unsatisified
-      <a className={styles.csnButtonLikeLink} href="https://metamask.io/" target="_blank" rel="noreferrer">
-        Try MetaMask
-      </a>
-    </div>;
-  }
+  // function getEthNeededPanel() {
+  //   return <div className="csn-eth-panel">
+  //     You need wallet or you will stay unsatisified
+  //     <a className={styles.csnButtonLikeLink} href="https://metamask.io/" target="_blank" rel="noreferrer">
+  //       Try MetaMask
+  //     </a>
+  //   </div>;
+  // }
 
-  function getWaitingForEthPanel() {
-    return <div className="csn-no-eth-accounts">
-      Waiting for Ethereum accounts...
-    </div>;
-  }
+  // function getWaitingForEthPanel() {
+  //   return <div className="csn-no-eth-accounts">
+  //     Waiting for Ethereum accounts...
+  //   </div>;
+  // }
 
-  function getAppPanel() {
-    return <div className={styles.csnApp}>
-      <h1>Ceramic is here</h1>
-      {getTestDocUI(testDoc, streamId)}
-      <div>
-        <DataModels ceramic={ceramic} ethereum={ethereum} />
-      </div>
-    </div>;
-  }
+  // function getAppPanel() {
+  //   return <div className={styles.csnApp}>
+  //     <h1>Ceramic is here</h1>
+  //     {getTestDocUI(testDoc, streamId)}
+  //     <div>
+  //       <DataModels ceramic={ceramic} ethereum={ethereum} />
+  //     </div>
+  //   </div>;
+  // }
 
-  function getWaitingForDIDPanel() {
-    return <div className="csn-waiting-for-did">
-      Waiting for a decentralized ID...
-    </div>
-  }
+  // function getWaitingForDIDPanel() {
+  //   return <div className="csn-waiting-for-did">
+  //     Waiting for a decentralized ID...
+  //   </div>
+  // }
 
-  function getLandingPage() {
-    return (
-      <div className={styles.csnLandingPage}>
-        <div className={styles.csnTopBar}>
-          <div className={styles.csnLogoPanel}>
-            <h1 className={styles.csnLogoPanelTopHeading}>
-              Need Work?
-            </h1>
-            <div className={styles.csnLogoPanelImage}>
-              <Image alt="Job Slap Logo" title="Job Slap Logo" width="200" height="150" layout="intrinsic" src="/job-slap-1-200.png" />
-            </div>
-            <h1 className={styles.csnLogoPanelBottomHeading}>
-              We got your app
-            </h1>
-          </div>
-        </div>
-        <div className={styles.csnMainBar}>
-          <div className={styles.csnMainPanelContainer}>
-          <div className={styles.csnMainProblemPanel  + ' ' + styles.csnMainPanel }>
-              <div className={styles.csnMainProblemTitle }>
-                Problem
-              </div>
-              <div className={styles.csnMainProblemContent}>
-                <ul className={styles.csnMainList}>
-                  <li>Unsatisfying job?</li>
-                  <li>Feeling underappreciated?</li>
-                  <li>Can&apos;t see a way out?</li>
-                  <li><span className={styles.strikeout}>Maybe it&apos;s you?</span></li>
-                  <li>It&apos;s definitely NOT you!</li>
-                </ul>
-              </div>
-            </div>
-            <div className={styles.csnMainCenterPanel  + ' ' + styles.csnMainPanel }>
-              {
-                ethereum ?
-                (
-                  ethAddresses ?
-                  (
-                    ceramic ?
-                    <button onClick={e => setAppStarted(true)}>Let&apos;s go!!</button> :
-                    getWaitingForDIDPanel()
-                  )
-                  :
-                  getWaitingForEthPanel()
-                )
-                :
-                getEthNeededPanel()
-              }
-            </div>
-            <div className={styles.csnMainSolutionPanel + ' ' + styles.csnMainPanel }>
-              <div className={styles.csnMainSolutionTitle }>
-                Solution
-              </div>
-              <div className={styles.csnMainSolutionContent}>
-                <ul className={styles.csnMainList}>
-                  <li>Your skills are held captive by centralized mega-corporations</li>
-                  <li>Within every human lives magical potential</li>
-                  <li>Identify your identities</li>
-                  <li>Take control of your skills</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+  // function getLandingPage() {
+  //   return (
+  //     <div className={styles.csnLandingPage}>
+  //       <div className={styles.csnTopBar}>
+  //         <div className={styles.csnLogoPanel}>
+  //           <h1 className={styles.csnLogoPanelTopHeading}>
+  //             Need Work?
+  //           </h1>
+  //           <div className={styles.csnLogoPanelImage}>
+  //             <Image alt="Job Slap Logo" title="Job Slap Logo" width="200" height="150" layout="intrinsic" src="/job-slap-1-200.png" />
+  //           </div>
+  //           <h1 className={styles.csnLogoPanelBottomHeading}>
+  //             We got your app
+  //           </h1>
+  //         </div>
+  //       </div>
+  //       <div className={styles.csnMainBar}>
+  //         <div className={styles.csnMainPanelContainer}>
+  //         <div className={styles.csnMainProblemPanel  + ' ' + styles.csnMainPanel }>
+  //             <div className={styles.csnMainProblemTitle }>
+  //               Problem
+  //             </div>
+  //             <div className={styles.csnMainProblemContent}>
+  //               <ul className={styles.csnMainList}>
+  //                 <li>Unsatisfying job?</li>
+  //                 <li>Feeling underappreciated?</li>
+  //                 <li>Can&apos;t see a way out?</li>
+  //                 <li><span className={styles.strikeout}>Maybe it&apos;s you?</span></li>
+  //                 <li>It&apos;s definitely NOT you!</li>
+  //               </ul>
+  //             </div>
+  //           </div>
+  //           <div className={styles.csnMainCenterPanel  + ' ' + styles.csnMainPanel }>
+  //             {
+  //               ethereum ?
+  //               (
+  //                 ethAddresses ?
+  //                 (
+  //                   ceramic ?
+  //                   <button onClick={e => setAppStarted(true)}>Let&apos;s go!!</button> :
+  //                   getWaitingForDIDPanel()
+  //                 )
+  //                 :
+  //                 getWaitingForEthPanel()
+  //               )
+  //               :
+  //               getEthNeededPanel()
+  //             }
+  //           </div>
+  //           <div className={styles.csnMainSolutionPanel + ' ' + styles.csnMainPanel }>
+  //             <div className={styles.csnMainSolutionTitle }>
+  //               Solution
+  //             </div>
+  //             <div className={styles.csnMainSolutionContent}>
+  //               <ul className={styles.csnMainList}>
+  //                 <li>Your skills are held captive by centralized mega-corporations</li>
+  //                 <li>Within every human lives magical potential</li>
+  //                 <li>Identify your identities</li>
+  //                 <li>Take control of your skills</li>
+  //               </ul>
+  //             </div>
+  //           </div>
+  //         </div>
 
-        </div>
-        <div className={styles.csnFeaturesBar}>
-          <div className={styles.csnFeatures}>
-            <div className={styles.csnMainSolutionMagic}>
-              With Job Slap your magic will fly free and you will become satisfied
-            </div>
-          </div>
-        </div>
-    </div>
-    );
-  }
+  //       </div>
+  //       <div className={styles.csnFeaturesBar}>
+  //         <div className={styles.csnFeatures}>
+  //           <div className={styles.csnMainSolutionMagic}>
+  //             With Job Slap your magic will fly free and you will become satisfied
+  //           </div>
+  //         </div>
+  //       </div>
+  //   </div>
+  //   );
+  // }
 
-  function getSkillsPage() {
-    return <DataModels ceramic={ceramic} ethereum={ethereum} setAppStarted={setAppStarted} />
+  // function getSkillsPage() {
+  //   return <DataModels ceramic={ceramic} ethereum={ethereum} setAppStarted={setAppStarted} />
+  // }
+
+  function renderProjects() {
+    return <Projects />
   }
 
   return (
-    <div className={styles.csnApp}>
+    <div>
       {
-        (ceramic && appStarted) ?
-          getSkillsPage() :
-          getLandingPage()
+        // (ceramic) ?
+        renderProjects()
+
       }
     </div>
 
